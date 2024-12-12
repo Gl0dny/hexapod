92479ec7578b1158b0e428afee3492317af28cb5
Gl0dny <krystian.glodek1717@gmail.com>
2024-12-13 00:19:14 +0100

    update lights colors

--- src/lights/lights.py
+++ src/lights/lights.py
@@ -4,17 +4,18 @@ from .apa102 import APA102
 class Lights:
     COLORS_RGB = {
     'blue': (0, 0, 255),
+    'teal': (0, 128, 128),
     'green': (0, 255, 0),
-    'orange': (255, 50, 0),
+    'lime': (75, 180, 0),
+    'yellow': (255, 215, 0),
+    'golden': (255, 50, 0),
+    'orange': (255,25,0),
+    'red': (255, 0, 0),
     'pink': (255, 51, 183),
     'purple': (128, 0, 128),
-    'red': (255, 0, 0),
-    'white': (255, 255, 255),
-    'yellow': (255, 215, 0),
-    'brown': (139, 69, 19),
-    'gray': (128, 128, 128),
-    'teal': (0, 128, 128),
     'indigo': (75, 0, 130),
+    'gray': (139, 69, 19),
+    'white': (255, 255, 255)
     }
     
     def __init__(self, num_led=12, power_pin=5, initial_color='indigo'):
