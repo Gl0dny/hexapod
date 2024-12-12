0c4f29b9856dd582cca97374c0b10ade095e9891
Gl0dny <krystian.glodek1717@gmail.com>
2024-12-12 19:26:05 +0100

    intent handler and control logic templates added

--- src/control/control_module.py
+++ src/control/control_module.py
@@ -13,6 +13,31 @@ class ControlModule:
         self.lights = Lights()
         logger.info("ControlModule initialized with Lights.")
 
+    def move(self, direction):
+        logger.info(f"Executing move: {direction}")
+        # Implement movement logic here
+        # Example: Send command to servo controller
+        # Update state if using StateManager
+        # self.state_manager.set_state(RobotState.MOVING)
+
+    def stop(self):
+        logger.info("Executing stop.")
+        # Implement stop logic here
+
+    def idle_stance(self):
+        logger.info("Setting robot to idle stance.")
+        # Implement logic to set idle stance
+
+    def rotate(self, angle=None, direction=None):
+        if angle:
+            logger.info(f"Rotating robot by {angle} degrees.")
+            # Implement rotation logic based on angle
+        elif direction:
+            logger.info(f"Rotating robot to the {direction}.")
+            # Implement rotation logic based on direction
+        else:
+            logger.error("No angle or direction provided for rotation.")
+
     def turn_lights(self, switch_state):
         if switch_state == 'off':
             logger.info(f"Turning lights off")
@@ -28,21 +53,58 @@ class ControlModule:
         else:
             logger.error(f"Color '{color}' is not supported.")
 
-    def move(self, direction):
-        logger.info(f"Executing move: {direction}")
-        # Implement movement logic here
-        # Example: Send command to servo controller
-        # Update state if using StateManager
-        # self.state_manager.set_state(RobotState.MOVING)
+    def repeat_last_command(self):
+        logger.info("Repeating last command.")
+        # Implement logic to repeat the last command
 
-    def turn(self, direction):
-        logger.info(f"Executing turn: {direction}")
-        # Implement turning logic here
+    def say_hello(self):
+        logger.info("Saying hello.")
+        # Implement logic to say hello
 
-    def stop(self):
-        logger.info("Executing stop.")
-        # Implement stop logic here
+    def show_off(self):
+        logger.info("Performing show-off routine.")
+        # Implement show-off routine
 
-    def change_mode(self, mode):
-        logger.info(f"Changing mode to: {mode}")
-        # Implement mode change logic here
+    def dance(self):
+        logger.info("Performing dance routine.")
+        # Implement dance routine
+
+    def set_speed(self, speed_percentage):
+        logger.info(f"Setting speed to {speed_percentage}%.")
+        # Implement logic to set speed
+
+    def set_acceleration(self, accel_percentage):
+        logger.info(f"Setting acceleration to {accel_percentage}%.")
+        # Implement logic to set acceleration
+
+    def set_brightness(self, brightness_percentage):
+        logger.info(f"Setting brightness to {brightness_percentage}%.")
+        # Implement logic to set lights brightness
+
+    def shut_down(self):
+        logger.info("Shutting down robot.")
+        # Implement shutdown logic
+
+    def wake_up(self):
+        logger.info("Waking up robot.")
+        # Implement wake-up logic
+
+    def sleep(self):
+        logger.info("Putting robot to sleep.")
+        # Implement sleep logic
+
+    def set_low_profile_mode(self):
+        logger.info("Setting robot to low profile mode.")
+        # Implement logic to set low profile mode
+
+    def set_upright_mode(self):
+        logger.info("Setting robot to upright mode.")
+        # Implement logic to set upright mode
+
+    def helix(self):
+        logger.info("Performing helix maneuver.")
+        # Implement helix maneuver
+
+    # def change_mode(self, mode):
+    #     logger.info(f"Changing mode to: {mode}")
+    #     # Implement mode change logic here
--- src/kws/intent_dispatcher.py
+++ src/kws/intent_dispatcher.py
@@ -7,11 +7,25 @@ class IntentDispatcher:
         self.control = control_module
         self.intent_handlers = {
             'move': self.handle_move,
-            'turn': self.handle_turn,
             'stop': self.handle_stop,
-            'mode': self.handle_mode,
+            'idle_stance': self.handle_idle_stance,
+            'rotate': self.handle_turn,
             'turn_lights': self.handle_turn_lights,
-            'change_color': self.handle_change_color
+            'change_color': self.handle_change_color,
+            'repeat': self.handle_repeat,
+            'hello': self.handle_hello,
+            'show_off': self.handle_show_off,
+            'dance': self.handle_dance,
+            'set_speed': self.handle_set_speed,
+            'set_accel': self.handle_set_accel,
+            'set_brightness': self.handle_set_brightness,
+            'shut_down': self.handle_shut_down,
+            'wake_up': self.handle_wake_up,
+            'sleep': self.handle_sleep,
+            'low_profle_mode': self.handle_low_profile_mode,
+            'upright_mode': self.handle_upright_mode,
+            'helix': self.handle_helix
+            # 'mode': self.handle_mode,
         }
 
     def dispatch(self, intent, slots):
@@ -29,25 +43,25 @@ class IntentDispatcher:
         else:
             logger.error("No direction provided for move command.")
 
-    def handle_turn(self, slots):
-        turn_direction = slots.get('turn_direction')
-        if turn_direction:
-            logger.info(f"Handling turn command: {turn_direction}")
-            self.control.turn(turn_direction)
-        else:
-            logger.error("No turn_direction provided for turn command.")
-
     def handle_stop(self, slots):
         logger.info("Handling stop command.")
         self.control.stop()
 
-    def handle_mode(self, slots):
-        mode = slots.get('mode')
-        if mode:
-            logger.info(f"Handling mode command: {mode}")
-            self.control.change_mode(mode)
+    def handle_idle_stance(self, slots):
+        logger.info("Handling idle_stance command.")
+        self.control.idle_stance()
+
+    def handle_rotate(self, slots):
+        angle = slots.get('angle')
+        turn_direction = slots.get('turn_direction')
+        if angle:
+            logger.info(f"Handling rotate command with angle: {angle}")
+            self.control.rotate(angle=angle)
+        elif turn_direction:
+            logger.info(f"Handling rotate command with direction: {turn_direction}")
+            self.control.rotate(direction=turn_direction)
         else:
-            logger.error("No mode provided for mode command.")
+            logger.error("No angle or turn_direction provided for rotate command.")
 
     def handle_turn_lights(self, slots):
         switch_state = slots.get('switch_state')
@@ -58,3 +72,75 @@ class IntentDispatcher:
         color = slots.get('color')
         logger.info(f"Handling change_color command: {color}")
         self.control.change_color(color)
+
+    def handle_repeat(self, slots):
+        logger.info("Handling repeat command.")
+        self.control.repeat_last_command()
+
+    def handle_hello(self, slots):
+        logger.info("Handling hello command.")
+        self.control.say_hello()
+
+    def handle_show_off(self, slots):
+        logger.info("Handling show_off command.")
+        self.control.show_off()
+
+    def handle_dance(self, slots):
+        logger.info("Handling dance command.")
+        self.control.dance()
+
+    def handle_set_speed(self, slots):
+        speed_percentage = slots.get('speed_percentage')
+        if speed_percentage:
+            logger.info(f"Handling set_speed command: {speed_percentage}%")
+            self.control.set_speed(speed_percentage)
+        else:
+            logger.error("No speed_percentage provided for set_speed command.")
+
+    def handle_set_accel(self, slots):
+        accel_percentage = slots.get('accel_percentage')
+        if accel_percentage:
+            logger.info(f"Handling set_accel command: {accel_percentage}%")
+            self.control.set_acceleration(accel_percentage)
+        else:
+            logger.error("No accel_percentage provided for set_accel command.")
+
+    def handle_set_brightness(self, slots):
+        brightness_percentage = slots.get('brightness_percentage')
+        if brightness_percentage:
+            logger.info(f"Handling set_brightness command: {brightness_percentage}%")
+            self.control.set_brightness(brightness_percentage)
+        else:
+            logger.error("No brightness_percentage provided for set_brightness command.")
+
+    def handle_shut_down(self, slots):
+        logger.info("Handling shut_down command.")
+        self.control.shut_down()
+
+    def handle_wake_up(self, slots):
+        logger.info("Handling wake_up command.")
+        self.control.wake_up()
+
+    def handle_sleep(self, slots):
+        logger.info("Handling sleep command.")
+        self.control.sleep()
+
+    def handle_low_profile_mode(self, slots):
+        logger.info("Handling low_profile_mode command.")
+        self.control.set_low_profile_mode()
+
+    def handle_upright_mode(self, slots):
+        logger.info("Handling upright_mode command.")
+        self.control.set_upright_mode()
+
+    def handle_helix(self, slots):
+        logger.info("Handling helix command.")
+        self.control.helix()
+
+    # def handle_mode(self, slots):
+    #     mode = slots.get('mode')
+    #     if mode:
+    #         logger.info(f"Handling mode command: {mode}")
+    #         self.control.change_mode(mode)
+    #     else:
+    #         logger.error("No mode provided for mode command.")
