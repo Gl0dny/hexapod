ed375cb3d24a4143059622ea486df74effc65486
Gl0dny <krystian.glodek1717@gmail.com>
2024-12-16 01:46:13 +0100

    imu unit tests added

---
+++ tests/imu/test_imu.py
@@ -0,0 +1,48 @@
+from unittest.mock import patch
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src/')))
+
+from imu import Imu
+
+@patch('imu.imu.ICM20948')
+def test_get_acceleration(mock_icm):
+    mock_instance = mock_icm.return_value
+    mock_instance.read_accelerometer_gyro_data.return_value = (0.01, 0.01, 1.01, 0.46, 1.14, -0.35)
+    
+    imu = Imu()
+    ax, ay, az = imu.get_acceleration()
+    assert ax == 0.01
+    assert ay == 0.01
+    assert az == 1.01
+
+@patch('imu.imu.ICM20948')
+def test_get_gyroscope(mock_icm):
+    mock_instance = mock_icm.return_value
+    mock_instance.read_accelerometer_gyro_data.return_value = (0.01, 0.01, 1.01, 0.46, 1.14, -0.35)
+    
+    imu = Imu()
+    gx, gy, gz = imu.get_gyroscope()
+    assert gx == 0.46
+    assert gy == 1.14
+    assert gz == -0.35
+
+@patch('imu.imu.ICM20948')
+def test_get_magnetometer(mock_icm):
+    mock_instance = mock_icm.return_value
+    mock_instance.read_magnetometer_data.return_value = (-4.95, -6.30, 103.95)
+    
+    imu = Imu()
+    mx, my, mz = imu.get_magnetometer()
+    assert mx == -4.95
+    assert my == -6.30
+    assert mz == 103.95
+
+@patch('imu.imu.ICM20948')
+def test_get_temperature(mock_icm):
+    mock_instance = mock_icm.return_value
+    mock_instance.read_temperature.return_value = 33.11
+    
+    imu = Imu()
+    temp = imu.get_temperature()
+    assert temp == 33.11
