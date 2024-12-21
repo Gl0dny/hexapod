ebad43cf4c469bd5b515854a9fed7e0f77363e90
Gl0dny <krystian.glodek1717@gmail.com>
2024-12-21 21:17:03 +0100

    refactor lights tests to use class-based structure for improved organization and clarity

--- tests/lights/test_animation.py
+++ tests/lights/test_animation.py
@@ -35,94 +35,96 @@ def setup_animation(mocker, mock_lights):
     mocker.patch.object(mock_lights, 'clear')
     return mock_lights
 
-def test_alternate_rotate_animation_run(mocker, setup_animation):
-    mock_lights = setup_animation
-    animation = AlternateRotateAnimation(mock_lights)
-    mocker.patch.object(animation.stop_event, 'wait', side_effect=[False]*animation.lights.num_led*2 + [True])  # returns False for each LED and then True once the animation should stop
-    animation.start()
-    animation.stop_animation()
-    assert mock_lights.set_color.call_count == mock_lights.num_led
-    assert mock_lights.rotate.call_count == mock_lights.rotate_positions
+class TestAnimations:
 
-def test_wheel_fill_animation_run(mocker, setup_animation):
-    mock_lights = setup_animation
-    animation = WheelFillAnimation(mock_lights)
-    mocker.patch.object(animation.stop_event, 'wait', side_effect=[False]*animation.lights.num_led + [True]) # returns False for each LED and then True once the animation should stop
-    animation.start()
-    animation.stop_animation()
-    assert mock_lights.get_wheel_color.call_count == mock_lights.num_led
-    assert mock_lights.set_color_rgb.call_count == mock_lights.num_led
+    def test_alternate_rotate_animation_run(self, mocker, setup_animation):
+        mock_lights = setup_animation
+        animation = AlternateRotateAnimation(mock_lights)
+        mocker.patch.object(animation.stop_event, 'wait', side_effect=[False]*animation.lights.num_led*2 + [True])
+        animation.start()
+        animation.stop_animation()
+        assert mock_lights.set_color.call_count == mock_lights.num_led
+        assert mock_lights.rotate.call_count == mock_lights.rotate_positions
 
-def test_pulse_smoothly_animation_run(mocker, setup_animation):
-    mock_lights = setup_animation
-    animation = PulseSmoothlyAnimation(mock_lights)
-    mocker.patch.object(animation.stop_event, 'wait', side_effect=[False, False, True])  # Allow two iterations
-    animation.start()
-    animation.stop_animation()
-    assert mock_lights.set_color_rgb.call_count == 2  # Assert alternation between base and pulse colors
+    def test_wheel_fill_animation_run(self, mocker, setup_animation):
+        mock_lights = setup_animation
+        animation = WheelFillAnimation(mock_lights)
+        mocker.patch.object(animation.stop_event, 'wait', side_effect=[False]*animation.lights.num_led + [True])
+        animation.start()
+        animation.stop_animation()
+        assert mock_lights.get_wheel_color.call_count == mock_lights.num_led
+        assert mock_lights.set_color_rgb.call_count == mock_lights.num_led
 
-def test_pulse_animation_run(mocker, setup_animation):
-    mock_lights = setup_animation
-    animation = PulseAnimation(mock_lights)
-    mocker.patch.object(animation.stop_event, 'wait', side_effect=[False, False, True]) # Allow two iterations
-    animation.start()
-    animation.stop_animation()
-    assert mock_lights.set_color.call_count == 2  # Assert alternation between base and pulse colors
+    def test_pulse_smoothly_animation_run(self, mocker, setup_animation):
+        mock_lights = setup_animation
+        animation = PulseSmoothlyAnimation(mock_lights)
+        mocker.patch.object(animation.stop_event, 'wait', side_effect=[False, False, True])
+        animation.start()
+        animation.stop_animation()
+        assert mock_lights.set_color_rgb.call_count == 2
 
-def test_wheel_animation_run(mocker, setup_animation):
-    mock_lights = setup_animation
-    animation = WheelAnimation(mock_lights)
-    mocker.patch.object(animation.stop_event, 'wait', side_effect=[False]*animation.lights.num_led + [True]) # returns False for each LED and then True once the animation should stop.
-    animation.start()
-    animation.stop_animation()
-    assert mock_lights.get_wheel_color.call_count == mock_lights.num_led
-    assert mock_lights.clear.call_count == mock_lights.num_led
-    assert mock_lights.set_color_rgb.call_count == mock_lights.num_led
+    def test_pulse_animation_run(self, mocker, setup_animation):
+        mock_lights = setup_animation
+        animation = PulseAnimation(mock_lights)
+        mocker.patch.object(animation.stop_event, 'wait', side_effect=[False, False, True])
+        animation.start()
+        animation.stop_animation()
+        assert mock_lights.set_color.call_count == 2
 
-def test_opposite_rotate_animation_run(mocker, setup_animation):
-    mock_lights = setup_animation
-    animation = OppositeRotateAnimation(mock_lights)
-    max_offset = mock_lights.num_led // 2
-    rotations = (1 + (max_offset-1) + 1)*2
-    loops_number=[False]*rotations + [True]
-    mocker.patch.object(animation.stop_event, 'wait', side_effect = loops_number)
-    animation.start()
-    animation.stop_animation()
-    assert mock_lights.set_color.call_count == (1 + 2*(max_offset-1) + 1)*2
-    assert mock_lights.clear.call_count == rotations
+    def test_wheel_animation_run(self, mocker, setup_animation):
+        mock_lights = setup_animation
+        animation = WheelAnimation(mock_lights)
+        mocker.patch.object(animation.stop_event, 'wait', side_effect=[False]*animation.lights.num_led + [True])
+        animation.start()
+        animation.stop_animation()
+        assert mock_lights.get_wheel_color.call_count == mock_lights.num_led
+        assert mock_lights.clear.call_count == mock_lights.num_led
+        assert mock_lights.set_color_rgb.call_count == mock_lights.num_led
 
-def test_alternate_rotate_animation_init(mock_lights):
-    animation = AlternateRotateAnimation(mock_lights, color_even='red', color_odd='blue', delay=0.5, positions=10)
-    assert animation.color_even == 'red'
-    assert animation.color_odd == 'blue'
-    assert animation.delay == 0.5
-    assert animation.positions == 10
+    def test_opposite_rotate_animation_run(self, mocker, setup_animation):
+        mock_lights = setup_animation
+        animation = OppositeRotateAnimation(mock_lights)
+        max_offset = mock_lights.num_led // 2
+        rotations = (1 + (max_offset-1) + 1)*2
+        loops_number=[False]*rotations + [True]
+        mocker.patch.object(animation.stop_event, 'wait', side_effect = loops_number)
+        animation.start()
+        animation.stop_animation()
+        assert mock_lights.set_color.call_count == (1 + 2*(max_offset-1) + 1)*2
+        assert mock_lights.clear.call_count == rotations
 
-def test_wheel_fill_animation_init(mock_lights):
-    animation = WheelFillAnimation(mock_lights, use_rainbow=False, color='green', interval=0.3)
-    assert not animation.use_rainbow
-    assert animation.color == 'green'
-    assert animation.interval == 0.3
+    def test_alternate_rotate_animation_init(self, mock_lights):
+        animation = AlternateRotateAnimation(mock_lights, color_even='red', color_odd='blue', delay=0.5, positions=10)
+        assert animation.color_even == 'red'
+        assert animation.color_odd == 'blue'
+        assert animation.delay == 0.5
+        assert animation.positions == 10
 
-def test_pulse_smoothly_animation_init(mock_lights):
-    animation = PulseSmoothlyAnimation(mock_lights, base_color='yellow', pulse_color='purple', pulse_speed=0.1)
-    assert animation.base_color == 'yellow'
-    assert animation.pulse_color == 'purple'
-    assert animation.pulse_speed == 0.1
+    def test_wheel_fill_animation_init(self, mock_lights):
+        animation = WheelFillAnimation(mock_lights, use_rainbow=False, color='green', interval=0.3)
+        assert not animation.use_rainbow
+        assert animation.color == 'green'
+        assert animation.interval == 0.3
 
-def test_pulse_animation_init(mock_lights):
-    animation = PulseAnimation(mock_lights, base_color='white', pulse_color='black', pulse_speed=0.2)
-    assert animation.base_color == 'white'
-    assert animation.pulse_color == 'black'
-    assert animation.pulse_speed == 0.2
+    def test_pulse_smoothly_animation_init(self, mock_lights):
+        animation = PulseSmoothlyAnimation(mock_lights, base_color='yellow', pulse_color='purple', pulse_speed=0.1)
+        assert animation.base_color == 'yellow'
+        assert animation.pulse_color == 'purple'
+        assert animation.pulse_speed == 0.1
 
-def test_wheel_animation_init(mock_lights):
-    animation = WheelAnimation(mock_lights, use_rainbow=True, color='orange', interval=0.25)
-    assert animation.use_rainbow
-    assert animation.color == 'orange'
-    assert animation.interval == 0.25
+    def test_pulse_animation_init(self, mock_lights):
+        animation = PulseAnimation(mock_lights, base_color='white', pulse_color='black', pulse_speed=0.2)
+        assert animation.base_color == 'white'
+        assert animation.pulse_color == 'black'
+        assert animation.pulse_speed == 0.2
 
-def test_opposite_rotate_animation_init(mock_lights):
-    animation = OppositeRotateAnimation(mock_lights, interval=0.15, color='cyan')
-    assert animation.interval == 0.15
-    assert animation.color == 'cyan'
+    def test_wheel_animation_init(self, mock_lights):
+        animation = WheelAnimation(mock_lights, use_rainbow=True, color='orange', interval=0.25)
+        assert animation.use_rainbow
+        assert animation.color == 'orange'
+        assert animation.interval == 0.25
+
+    def test_opposite_rotate_animation_init(self, mock_lights):
+        animation = OppositeRotateAnimation(mock_lights, interval=0.15, color='cyan')
+        assert animation.interval == 0.15
+        assert animation.color == 'cyan'
--- tests/lights/test_lights.py
+++ tests/lights/test_lights.py
@@ -12,97 +12,92 @@ def lights_fixture(mocker):
     lights = Lights()
     return mock_apa102, mock_led, lights
 
-def test_init_lights(lights_fixture):
-    mock_apa102, mock_led, lights = lights_fixture
-    mock_apa102.assert_called_with(num_led=12)
-    mock_led.assert_called_with(5)
-    mock_led.return_value.on.assert_called_once()
-    assert lights.brightness == 50
-    assert lights.driver.global_brightness == int(0b11111 * 50 / 100)
+class TestLights:
 
-def test_set_brightness(lights_fixture):
-    _, _, lights = lights_fixture
-    lights.set_brightness(120)
-    assert lights.brightness == 100
-    assert lights.driver.global_brightness == 31
-    lights.set_brightness(-10)
-    assert lights.brightness == 0
-    assert lights.driver.global_brightness == 0
-    lights.set_brightness(50)
-    assert lights.driver.global_brightness == int(0b11111 * 50 / 100)
+    def test_init_lights(self, lights_fixture):
+        mock_apa102, mock_led, lights = lights_fixture
+        mock_apa102.assert_called_with(num_led=12)
+        mock_led.assert_called_with(5)
+        mock_led.return_value.on.assert_called_once()
+        assert lights.brightness == 50
+        assert lights.driver.global_brightness == int(0b11111 * 50 / 100)
 
-def test_set_color_rgb(lights_fixture):
-    _, _, lights = lights_fixture
-    lights.set_color_rgb((10, 20, 30))
-    for i in range(Lights.DEFAULT_NUM_LED):
-        lights.driver.set_pixel.assert_any_call(i, 10, 20, 30)
-    lights.driver.show.assert_called_once()
+    def test_set_brightness(self, lights_fixture):
+        _, _, lights = lights_fixture
+        lights.set_brightness(120)
+        assert lights.brightness == 100
+        assert lights.driver.global_brightness == 31
+        lights.set_brightness(-10)
+        assert lights.brightness == 0
+        assert lights.driver.global_brightness == 0
+        lights.set_brightness(50)
+        assert lights.driver.global_brightness == int(0b11111 * 50 / 100)
 
-def test_set_color_valid(lights_fixture):
-    _, _, lights = lights_fixture
-    lights.set_color('red')
-    for i in range(Lights.DEFAULT_NUM_LED):
-        lights.driver.set_pixel.assert_any_call(i, 255, 0, 0)
-    lights.driver.show.assert_called_once()
+    def test_set_color_rgb(self, lights_fixture):
+        _, _, lights = lights_fixture
+        lights.set_color_rgb((10, 20, 30))
+        for i in range(Lights.DEFAULT_NUM_LED):
+            lights.driver.set_pixel.assert_any_call(i, 10, 20, 30)
+        lights.driver.show.assert_called_once()
 
-def test_set_color_invalid(lights_fixture):
-    _, _, lights = lights_fixture
-    with pytest.raises(ValueError):
-        lights.set_color("magenta")
+    def test_set_color_valid(self, lights_fixture):
+        _, _, lights = lights_fixture
+        lights.set_color('red')
+        for i in range(Lights.DEFAULT_NUM_LED):
+            lights.driver.set_pixel.assert_any_call(i, 255, 0, 0)
+        lights.driver.show.assert_called_once()
 
-def test_set_color_invalid_input(lights_fixture):
-    _, _, lights = lights_fixture
-    with pytest.raises(ValueError):
-        lights.set_color(123)
-    
-    with pytest.raises(ValueError):
-        lights.set_color(None)
-    
-    with pytest.raises(ValueError):
-        lights.set_color(['red'])
-    
-    with pytest.raises(ValueError):
-        lights.set_color({'color': 'red'})
+    def test_set_color_invalid(self, lights_fixture):
+        _, _, lights = lights_fixture
+        with pytest.raises(ValueError):
+            lights.set_color("magenta")
 
-def test_set_color_rgb(lights_fixture):
-    _, _, lights = lights_fixture
-    lights.set_color_rgb((10, 20, 30))
-    for i in range(Lights.DEFAULT_NUM_LED):
-        lights.driver.set_pixel.assert_any_call(i, 10, 20, 30)
-    lights.driver.show.assert_called_once()
+    def test_set_color_invalid_input(self, lights_fixture):
+        _, _, lights = lights_fixture
+        with pytest.raises(ValueError):
+            lights.set_color(123)
+        
+        with pytest.raises(ValueError):
+            lights.set_color(None)
+        
+        with pytest.raises(ValueError):
+            lights.set_color(['red'])
+        
+        with pytest.raises(ValueError):
+            lights.set_color({'color': 'red'})
 
-def test_set_color_rgb_invalid(lights_fixture):
-    _, _, lights = lights_fixture
-    with pytest.raises(ValueError):
-        lights.set_color_rgb((255, 255))
-    
-    with pytest.raises(ValueError):
-        lights.set_color_rgb((255, '255', 255))
-    
-    with pytest.raises(ValueError):
-        lights.set_color_rgb((256, 255, 255))
-    
-    with pytest.raises(ValueError):
-        lights.set_color_rgb((-1, 255, 255))
-    
-    with pytest.raises(ValueError):
-        lights.set_color_rgb([255, 255, 255])
+    def test_set_color_rgb_invalid(self, lights_fixture):
+        _, _, lights = lights_fixture
+        with pytest.raises(ValueError):
+            lights.set_color_rgb((255, 255))
+        
+        with pytest.raises(ValueError):
+            lights.set_color_rgb((255, '255', 255))
+        
+        with pytest.raises(ValueError):
+            lights.set_color_rgb((256, 255, 255))
+        
+        with pytest.raises(ValueError):
+            lights.set_color_rgb((-1, 255, 255))
+        
+        with pytest.raises(ValueError):
+            lights.set_color_rgb([255, 255, 255])
 
-def test_rotate(lights_fixture):
-    _, _, lights = lights_fixture
-    lights.rotate(3)
-    lights.driver.rotate.assert_called_with(3)
-    lights.driver.show.assert_called()
+    def test_rotate(self, lights_fixture):
+        _, _, lights = lights_fixture
+        lights.rotate(3)
+        lights.driver.rotate.assert_called_with(3)
+        lights.driver.show.assert_called()
 
-def test_get_wheel_color(lights_fixture):
-    _, _, lights = lights_fixture
-    assert lights.get_wheel_color(0) == (0, 255, 0)
-    assert lights.get_wheel_color(85) == (255, 0, 0)
-    assert lights.get_wheel_color(170) == (0, 0, 255)
+    def test_get_wheel_color(self, lights_fixture):
+        _, _, lights = lights_fixture
+        assert lights.get_wheel_color(0) == (0, 255, 0)
+        assert lights.get_wheel_color(85) == (255, 0, 0)
+        assert lights.get_wheel_color(170) == (0, 0, 255)
 
-def test_clear(lights_fixture):
-    _, _, lights = lights_fixture
-    lights.clear(led_indices=[1, 2])
-    lights.driver.set_pixel.assert_any_call(1, 0, 0, 0)
-    lights.driver.set_pixel.assert_any_call(2, 0, 0, 0)
-    lights.driver.show.assert_called()
+    def test_clear(self, lights_fixture):
+        _, _, lights = lights_fixture
+        lights.clear(led_indices=[1, 2])
+        lights.driver.set_pixel.assert_any_call(1, 0, 0, 0)
+        lights.driver.set_pixel.assert_any_call(2, 0, 0, 0)
+        lights.driver.show.assert_called()
