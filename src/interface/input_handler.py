aa060b345b50fca0143d3acde425dfeac7dd85cf
Gl0dny <krystian.glodek1717@gmail.com>
2024-12-29 13:46:00 +0100

    non-block input implementation

--- src/interface/input_handler.py
+++ src/interface/input_handler.py
@@ -1,6 +1,7 @@
-
 import threading
 import queue
+import sys
+import select
 from typing import Optional
 
 class InputHandler:
@@ -10,22 +11,19 @@ class InputHandler:
         """
         self.input_queue = queue.Queue()
         self.stop_input_listener = False
+        self.current_input = ''
         self.input_thread = threading.Thread(target=self._input_listener, daemon=True)
         self.input_thread.start()
 
     def _input_listener(self):
         """
-        Continuously listens for user input and places it into the input queue.
+        Continuously listens for user input in a non-blocking manner and places it into the input queue.
         """
         while not self.stop_input_listener:
-            try:
-                user_input = input()
+            dr, dw, de = select.select([sys.stdin], [], [], 0.1)
+            if dr:
+                user_input = sys.stdin.readline().strip()
                 self.input_queue.put(user_input)
-            except EOFError:
-                # Handle end-of-file condition if needed
-                self.input_queue.put(None)
-            except Exception:
-                self.input_queue.put(None)
 
     def get_input(self, timeout: float = 0.1) -> Optional[str]:
         """
@@ -47,3 +45,4 @@ class InputHandler:
         Shuts down the input listener thread gracefully.
         """
         self.stop_input_listener = True
+        self.input_thread.join(timeout=0.01)
