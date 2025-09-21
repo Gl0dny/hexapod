e3fd8e5a6d2ddfcbed2fe79ea2e1c8b9aa727431
Gl0dny <krystian.glodek1717@gmail.com>
2025-09-21 22:06:58 +0200

    resampy llvm numba not necessary in default version

--- hexapod/odas/__init__.py
+++ hexapod/odas/__init__.py
@@ -1,4 +1,5 @@
-from .odas_audio_processor import ODASAudioProcessor
+# from .odas_audio_processor import ODASAudioProcessor #resampy, llvmlite, numba -> LLMV 15 installation needed
 from .odas_doa_ssl_processor import ODASDoASSLProcessor
 
-__all__ = ["ODASAudioProcessor", "ODASDoASSLProcessor"]
+# __all__ = ["ODASAudioProcessor", "ODASDoASSLProcessor"] #resampy, llvmlite, numba -> LLMV 15 installation needed
+__all__ = ["ODASDoASSLProcessor"]
